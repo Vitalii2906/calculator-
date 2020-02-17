2f49aa16f89c19494fc8bf8abe3c5ddbfd24cf4a
Vitaliy <60292069+One-of-the@users.noreply.github.com>
2020-02-17 23:41:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ var newPeople = document.getElementById("people").value;
 
 if(valueProcent == 5){
 	var result = (newAccount * 0.05) / newPeople;
-	score.innerHTML = "Tip Amount: " + result;
+	score.innerHTML = "Tip Amount: " + result + "$";
 }else if (valueProcent == 10){
 	var result = (newAccount * 0.1) / newPeople;
-	score.innerHTML = "Tip Amount: " + result;
+	score.innerHTML = "Tip Amount: " + result + "$";
 }else if (valueProcent == 15){
 	var result = (newAccount * 0.15) / newPeople;
 	score.innerHTML = "Tip Amount: " + result + "$";
