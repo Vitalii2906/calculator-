1a423ea5dbe475152200538f4ff4dd21ba097d57
Vitaliy <60292069+One-of-the@users.noreply.github.com>
2020-02-18 15:15:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ var newPeople = document.getElementById("people").value;
 
 if(valueProcent == 5){
 	var result = (newAccount * 0.05) / newPeople;
-	score.innerHTML = "Tip Amount: " + result + "$";
+	bill.innerHTML = result;
 }else if (valueProcent == 10){
 	var result = (newAccount * 0.1) / newPeople;
-	score.innerHTML = "Tip Amount: " + result + "$";
+	bill.innerHTML = result;
 }else if (valueProcent == 15){
 	var result = (newAccount * 0.15) / newPeople;
-	score.innerHTML = "Tip Amount: " + result + "$";
+	bill.innerHTML = result;
 }else {
 	console.log("Error");
 }
